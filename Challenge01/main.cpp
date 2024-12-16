24c5a95b8a85b5f935c4a5e390a5938ec1f4b37f
Adam Spivack <spivack@outlook.com>
2024-12-16 14:42:20 -0800

    Got clipp argument parser to work

--- Challenge01/main.cpp
+++ Challenge01/main.cpp
@@ -1,6 +1,29 @@
 #include <iostream>
+#include <string>
+#include <vector>
+#include <format>
+#include "..\external\clipp\include\clipp.h"
 
 int main(int argc, char *argv[])
 {
-    std::cout << "Hello world" << '/n';
+    using namespace clipp;
+    // using cout = std::cout;
+
+    std::string filename;
+    std::vector<std::string> guesses;
+
+    auto cli = (required("--load") & value("file", filename),
+                required("--guess") & values("guesses", guesses));
+
+    auto result = parse(argc, argv, cli);
+
+    if (not result)
+    {
+        std::cout << "Usage:\n"
+                  << usage_lines(cli, "challenge01");
+        return -1;
+    }
+
+    std::cout << "Filename: " << filename << '\n';
+    std::cout << std::format("Found: {}", guesses.size()) << "\n";
 }
