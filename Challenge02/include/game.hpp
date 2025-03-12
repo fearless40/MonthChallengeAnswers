ff54e0ed0121f13b4d1a9c3b7a8a9a2ac0b8f6d7
fearless40 <spivack@gmail.com>
2025-03-11 22:01:51 -0700

    Start adding code for filewriting. Learning nvim. Making this much slower...

--- Challenge02/include/game.hpp
+++ Challenge02/include/game.hpp
@@ -6,96 +6,76 @@
 #include <string>
 #include <vector>
 
-enum class Orientation
-{
-    Horizontal,
-    Vertical
-};
+enum class Orientation { Horizontal, Vertical };
 
 const int UNSET = -1;
 
 struct Game;
 struct Player;
 
-struct Ship
-{
-    uint32_t shiplength;
-    AABB location{UNSET, UNSET, UNSET, UNSET};
-    constexpr Orientation orientation() const noexcept
-    {
-        if ((location.x2 - location.x) > (location.y2 - location.y))
-            return Orientation::Horizontal;
+struct Ship {
+  uint32_t shiplength;
+  AABB location{UNSET, UNSET, UNSET, UNSET};
+  constexpr Orientation orientation() const noexcept {
+    if ((location.x2 - location.x) > (location.y2 - location.y))
+      return Orientation::Horizontal;
 
-        return Orientation::Vertical;
-    }
+    return Orientation::Vertical;
+  }
 
-    constexpr int row_size() const noexcept
-    {
-        return location.x2 - location.x;
-    }
+  constexpr int row_size() const noexcept { return location.x2 - location.x; }
 
-    constexpr int col_size() const noexcept
-    {
-        return location.y2 - location.y;
-    }
+  constexpr int col_size() const noexcept { return location.y2 - location.y; }
 
-    constexpr bool is_valid() const noexcept
-    {
-        return (row_size() == 0 && col_size() == shiplength - 1) || (row_size() == shiplength - 1 && col_size() == 0);
-    }
+  constexpr bool is_valid() const noexcept {
+    return (row_size() == 0 && col_size() == shiplength - 1) ||
+           (row_size() == shiplength - 1 && col_size() == 0);
+  }
 
-    constexpr uint32_t id() const noexcept
-    {
-        return shiplength;
-    }
+  constexpr uint32_t id() const noexcept { return shiplength; }
 };
 
-struct Player
-{
-    uint32_t id;
-    std::string name;
-    std::vector<Ship> ships;
+struct Player {
+  uint32_t id;
+  std::string name;
+  std::vector<Ship> ships;
 
-    constexpr bool check_if_position_collides(AABB position)
-    {
-        for (auto &ship : ships)
-        {
-            if (aabb_collision(position, ship.location))
-                return true;
-        }
-        return false;
+  constexpr bool check_if_position_collides(AABB position) {
+    for (auto &ship : ships) {
+      if (aabb_collision(position, ship.location))
+        return true;
     }
+    return false;
+  }
 
-    constexpr bool any_collisions()
-    {
-        return any_collision(ships, [](auto &ship) { return ship.location; });
-    }
+  constexpr bool any_collisions() {
+    return any_collision(ships, [](auto &ship) { return ship.location; });
+  }
 
-    std::optional<Ship> ship_at_position(std::uint16_t row, std::uint16_t col) const;
+  std::optional<Ship> ship_at_position(std::uint16_t row,
+                                       std::uint16_t col) const;
 
-    bool generate_random_ships(Game const &game, ErrorReport &report);
+  bool generate_random_ships(Game const &game, ErrorReport &report);
 };
 
-struct Game
-{
-    std::uint16_t rows;
-    std::uint16_t cols;
-    uint32_t minShipSize;
-    uint32_t maxShipSize;
-    std::vector<Player> players;
+struct Game {
+  std::uint16_t rows;
+  std::uint16_t cols;
+  uint32_t minShipSize;
+  uint32_t maxShipSize;
+  std::vector<Player> players;
 
-    constexpr static bool valid_row_col(std::uint16_t row, std::uint16_t col)
-    {
-        return row >= 0 && row < std::numeric_limits<std::uint16_t>::max() && col >= 0 &&
-               col < std::numeric_limits<std::uint16_t>::max();
-    }
+  constexpr static bool valid_row_col(std::uint16_t row, std::uint16_t col) {
+    return row >= 0 && row < std::numeric_limits<std::uint16_t>::max() &&
+           col >= 0 && col < std::numeric_limits<std::uint16_t>::max();
+  }
 
-    constexpr bool valid_pos(RowCol pos) const noexcept
-    {
-        return (pos.row >= 0 and pos.row <= rows) and (pos.col >= 0 and pos.col <= cols);
-    }
+  constexpr bool valid_pos(RowCol pos) const noexcept {
+    return (pos.row >= 0 and pos.row <= rows) and
+           (pos.col >= 0 and pos.col <= cols);
+  }
 
-    bool report_game_is_valid(ErrorReport &report) const noexcept;
+  bool report_game_is_valid(ErrorReport &report) const noexcept;
 };
 
 void print_player(Game const &, Player const &);
